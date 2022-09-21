6cccdea85283dca4bd2e597b28f61d86eb9758a8
KZhengsha <113391615+KZhengsha@users.noreply.github.com>
2022-09-21 12:35:56 -0700

    still working on multiple choice

--- main.ts
+++ main.ts
@@ -125,6 +125,46 @@ function gameOver (text: string) {
     game.showLongText(text, DialogLayout.Bottom)
     game.over(false)
 }
+function multipleChoice (choices: any[]) {
+    choice_A = sprites.create(img`
+        . . 3 . . 
+        . 3 . 3 . 
+        3 . . . 3 
+        3 3 3 3 3 
+        3 . . . 3 
+        `, SpriteKind.Player)
+    choice_A.setPosition(78, 58)
+    game.showLongText(choices[0], DialogLayout.Bottom)
+    choice_A.destroy()
+    choice_B = sprites.create(img`
+        2 2 2 . . 
+        2 . . 2 . 
+        2 2 2 . . 
+        2 . . 2 . 
+        2 2 2 . . 
+        `, SpriteKind.Player)
+    choice_B.setPosition(78, 58)
+    game.showLongText(choices[1], DialogLayout.Bottom)
+    choice_B.destroy()
+    choice_C = sprites.create(img`
+        . 4 4 4 . 
+        4 . . . 4 
+        4 . . . . 
+        4 . . . 4 
+        . 4 4 4 . 
+        `, SpriteKind.Player)
+    choice_C.setPosition(78, 58)
+    game.showLongText(choices[2], DialogLayout.Bottom)
+    choice_C.destroy()
+    choice = game.askForString("Make a choice! (A, B or C)")
+    while (!(choice == "A" || (choice == "B" || choice == "C"))) {
+        choice = game.askForString("Make a choice! (A, B or C)")
+    }
+}
+let choice = ""
+let choice_C: Sprite = null
+let choice_B: Sprite = null
+let choice_A: Sprite = null
 let mySprite: Sprite = null
 scene.setBackgroundImage(img`
     3333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333
